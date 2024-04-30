4458cfd19c7ebecfcbe94c317ff34647a5b51a0e
KaungMyat-K <kaungmyat.dev8@gmail.com>
2024-04-30 23:45:28 +0630

    add search book api

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -1,11 +1,15 @@
 package com.library.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.library.entity.BookEntity;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity,Long> {
     
+    Page<BookEntity> findByTitleContaining(@RequestParam("title") String title,Pageable pageable);
 }
