8526462aa1373dc3100c82970595c30f386f9d49
KaungMyat-K <kaungmyat.dev8@gmail.com>
2024-07-28 21:02:59 +0630

    add admin message end point

--- src/main/java/com/library/controller/MessageController.java
+++ src/main/java/com/library/controller/MessageController.java
@@ -3,12 +3,14 @@ package com.library.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.library.Utils.ExtractJwt;
+import com.library.dto.AdminQuestionDto;
 import com.library.entity.MessageEntity;
 import com.library.service.MessageService;
 
@@ -30,6 +32,15 @@ public class MessageController {
         messageService.postMessage(messageRequest, userEmail);
     }
 
+       @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value="Authorization") String token,@RequestBody AdminQuestionDto adminQuestionDto)throws Exception {
+        String userEmail = extractJwt.payloadJWTExtraction(token, "\"sub\"");
+        String admin = extractJwt.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        messageService.putMessage(adminQuestionDto, userEmail);
+    }
 
 
 }
